2fe39027af278956608988bbd2ddfe95b0b2f38f
estebanm <esteban.morgade@oasisent.com.ar>
2026-07-06 10:43:58 +1000

    Primer push con estructura base del proyecto, configuracion y git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("*****Bienvenido al sistema de gestión de inventario*****");
+Console.WriteLine();
+Console.WriteLine($"Versión: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".Net Version: {Environment.Version}");
+Console.WriteLine("Creamos carpeta src/");
