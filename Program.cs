3c07446286bf58c55488b60fa208fb7663db6bb9
estebanm <esteban.morgade@oasisent.com.ar>
2026-08-02 09:09:37 +1000

    Agregamos generador de reporte usando LINQ en clase GeneradorReporte.cs dentro de Infrastructure

--- Program.cs
+++ Program.cs
@@ -5,16 +5,37 @@ using InventarioApp.Infrastructure;
 
 Console.WriteLine("**** Bienvenido al sistema de inventario ****");
 
-var repository = new InMemoryProductoRepository();
+/*var repository = new InMemoryProductoRepository();
 var almacenamiento = new JsonInvetarioStorage();
-string ruta = "invetario_test.json";
+string ruta = "invetario_test.json";*/
 
-var laptop = ProductoFactory.Crear("Laptop", 1500.00m, 10, CategoriaProducto.Electronica);
-var mouse = ProductoFactory.Crear("Mouse", 25.00m, 50, CategoriaProducto.Electronica);
-var teclado = ProductoFactory.Crear("Teclado", 45.00m, 30, CategoriaProducto.Electronica);
-var silla = ProductoFactory.Crear("Silla de oficina", 120.00m, 20, CategoriaProducto.Muebles);
-var escritorio = ProductoFactory.Crear("Escritorio", 250.00m, 15, CategoriaProducto.Muebles);
+var productos = new List<Producto>
+{
+    ProductoFactory.Crear("Laptop", 1500.00m, 10, CategoriaProducto.Electronica),
+    ProductoFactory.Crear("Mouse", 25.00m, 50, CategoriaProducto.Electronica),
+    ProductoFactory.Crear("Teclado", 45.00m, 30, CategoriaProducto.Electronica),
+    ProductoFactory.Crear("Silla de oficina", 120.00m, 20, CategoriaProducto.Muebles),
+    ProductoFactory.Crear("Escritorio", 250.00m, 15, CategoriaProducto.Muebles)
+};
 
+var generador = new GeneradorReportes(productos);
+
+Console.WriteLine(generador.GenerarResumen());
+Console.WriteLine("\n");
+
+Console.WriteLine(generador.GenerarReporteStockBajo());
+Console.WriteLine("\n");
+
+Console.WriteLine(generador.GenerarTopProductos());
+Console.WriteLine("\n");
+
+Console.WriteLine(generador.ExportCsv());
+Console.WriteLine("\n");
+
+Console.WriteLine(generador.ExportarResumenJson());
+
+
+/*
 repository.Agregar(laptop);
 repository.Agregar(mouse);
 repository.Agregar(teclado);
@@ -25,7 +46,7 @@ Console.WriteLine($"Productos agregados al inventario: {repository.Cantidad}\n")
 
 // Consultas basicas LINQ
 
-/*var electronicos = repository.BuscarPorCategoria(CategoriaProducto.Electronica);
+var electronicos = repository.BuscarPorCategoria(CategoriaProducto.Electronica);
 Console.WriteLine("Productos de electrónica:");
 foreach (var producto in electronicos)
 {
@@ -44,7 +65,7 @@ Console.WriteLine($"\nTodos los nombres de productos en el inventario: {string.J
 
 var hayStockBajo = repository.HayStockBajo();
 Console.WriteLine($"\n¿Hay productos con stock bajo? {(hayStockBajo ? "Sí" : "No")}");
-*/
+
 
 almacenamiento.CrearBackup(ruta);
 almacenamiento.Guardar(repository.ObtenerTodos(), ruta);
@@ -59,3 +80,4 @@ foreach (var p in productosCargardos)
 {
     Console.WriteLine($"ID: {p.Id}, Nombre: {p.Nombre}, Precio: {p.Precio}, Cantidad: {p.Cantidad}, Categoria: {p.Categoria}, Estado: {p.Estado}");
 }
+*/
